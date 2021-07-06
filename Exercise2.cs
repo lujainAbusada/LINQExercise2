2b1eddcfe7c0874795c860f0744ff77e07fd9fe7
Lujain Abusada <Lujain.Abusada@gmail.com>
2021-07-06 10:54:30 +0300

    excercise2 implementation

--- Exercise2.cs
+++ Exercise2.cs
@@ -8,10 +8,29 @@ namespace LINQExercise2
     {
         static void Main(string[] args)
         {
-            
+            List<string> words = new List<string> { "we", "They", "Hi", "Car", "She", "True" };
+            String lastWord = GetTheLastWord(words);
+
+            if (IsLastWordNull(lastWord))
+                Console.WriteLine(lastWord);
+            else
+                Console.WriteLine("The last word is {0}", lastWord);
         }
 
-        
+        public static string GetTheLastWord(IEnumerable<string> words)
+        {
+            string LastWord = words.Where(word => word.Contains('e'))
+                                    .OrderBy(word => word)
+                                    .LastOrDefault();
+            return LastWord;
+
+        }
+        public static bool IsLastWordNull(String LastWord)
+        {
+            return LastWord == null;
+        }
+
+
     }
 
 }
