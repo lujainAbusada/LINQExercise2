3b569596382c80686b3f34a8471efb8c246c380a
Lujain Abusada <Lujain.Abusada@gmail.com>
2021-07-06 10:51:54 +0300

    This project aims to find the last word that has the letter e in a sored list

---
+++ Exercise2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQExercise2
+{
+    class Exercise2
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+    }
+
+}
